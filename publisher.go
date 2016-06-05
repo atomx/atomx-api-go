aec51b03e51f7ef367d1fb9426574ddca9510c9a
Erik Dubbelboer <erik@dubbelboer.com>
2016-06-05 09:43:42 +0000

    Added publishers.RevShare

--- publisher.go
+++ publisher.go
@@ -1,8 +1,9 @@
 package atomx
 
 type Publisher struct {
-	ID      int64            `json:"id"`
-	State   string           `json:"state,omitempty"`
-	Name    string           `json:"name,omitempty"`
-	Network *NetworkRelation `json:"network,omitempty"`
+	ID       int64            `json:"id"`
+	State    string           `json:"state,omitempty"`
+	Name     string           `json:"name,omitempty"`
+	RevShare float64          `json:"revshare,omitempty"`
+	Network  *NetworkRelation `json:"network,omitempty"`
 }
