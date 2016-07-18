4e28a47c5d495d429b8d130e477d98d8403f322e
Simeon Mugisha <cmeo226@yahoo.com>
2016-07-18 19:47:25 +0800

    atomx-api-go: add fields for creatives

--- creative.go
+++ creative.go
@@ -1,13 +1,16 @@
 package atomx
 
 type Creative struct {
-	ID         int64               `json:"id"`
-	Advertiser *AdvertiserRelation `json:"advertiser,omitempty"`
-	Attributes CreativeAttributes  `json:"attributes,omitempty"`
-	AuditedAt  *Time               `json:"audited_at,omitempty"`
-	Bans       CreativeBans        `json:"bans,omitempty"`
-	Size       *SizeRelation       `json:"size,omitempty"`
-	SizeBytes  *int64              `json:"size_bytes,omitempty"`
-	Types      CreativeTypes       `json:"types,omitempty"`
-	FinalURL   string              `json:"final_url,omitempty"`
+	ID          int64               `json:"id"`
+	Name        string              `json:"name"`
+	Advertiser  *AdvertiserRelation `json:"advertiser,omitempty"`
+	Attributes  CreativeAttributes  `json:"attributes,omitempty"`
+	AuditedAt   *Time               `json:"audited_at,omitempty"`
+	Bans        CreativeBans        `json:"bans,omitempty"`
+	Size        *SizeRelation       `json:"size,omitempty"`
+	SizeBytes   *int64              `json:"size_bytes,omitempty"`
+	Types       CreativeTypes       `json:"types,omitempty"`
+	FinalURL    string              `json:"final_url,omitempty"`
+	ContentType string              `json:"content_type"`
+	Javascript  string              `json:"content_type"`
 }
