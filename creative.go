fe68c962a881ec2f3b1b92a64cbdcbf89f7039bf
Erik Dubbelboer <erik@dubbelboer.com>
2016-08-09 07:41:23 +0000

    Make all Creative fields optional

--- creative.go
+++ creative.go
@@ -2,7 +2,7 @@ package atomx
 
 type Creative struct {
 	ID          int64               `json:"id"`
-	Name        string              `json:"name"`
+	Name        *string             `json:"name,omitempty"`
 	Advertiser  *AdvertiserRelation `json:"advertiser,omitempty"`
 	Attributes  CreativeAttributes  `json:"attributes,omitempty"`
 	AuditedAt   *Time               `json:"audited_at,omitempty"`
@@ -10,8 +10,8 @@ type Creative struct {
 	Size        *SizeRelation       `json:"size,omitempty"`
 	SizeBytes   *int64              `json:"size_bytes,omitempty"`
 	Types       CreativeTypes       `json:"types,omitempty"`
-	FinalURL    string              `json:"final_url,omitempty"`
-	ContentType string              `json:"content_type,omitempty"`
-	Javascript  string              `json:"javascript,omitempty"`
+	FinalURL    *string             `json:"final_url,omitempty"`
+	ContentType *string             `json:"content_type,omitempty"`
+	Javascript  *string             `json:"javascript,omitempty"`
 	CheckedAt   *Time               `json:"checked_at,omitempty"`
 }
