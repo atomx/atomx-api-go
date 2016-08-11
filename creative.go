9c84f121a4d6c298fc37ea0aad89f9a19aa2542c
Erik Dubbelboer <erik@dubbelboer.com>
2016-08-11 04:37:30 +0000

    Added more fields to Creative

--- creative.go
+++ creative.go
@@ -1,17 +1,22 @@
 package atomx
 
 type Creative struct {
-	ID          int64               `json:"id"`
-	Name        *string             `json:"name,omitempty"`
-	Advertiser  *AdvertiserRelation `json:"advertiser,omitempty"`
-	Attributes  CreativeAttributes  `json:"attributes,omitempty"`
-	AuditedAt   *Time               `json:"audited_at,omitempty"`
-	Bans        CreativeBans        `json:"bans,omitempty"`
-	Size        *SizeRelation       `json:"size,omitempty"`
-	SizeBytes   *int64              `json:"size_bytes,omitempty"`
-	Types       CreativeTypes       `json:"types,omitempty"`
-	FinalURL    *string             `json:"final_url,omitempty"`
-	ContentType *string             `json:"content_type,omitempty"`
-	Javascript  *string             `json:"javascript,omitempty"`
-	CheckedAt   *Time               `json:"checked_at,omitempty"`
+	ID         int64               `json:"id"`
+	State      *string             `json:"state,omitempty"`
+	Name       *string             `json:"name,omitempty"`
+	Advertiser *AdvertiserRelation `json:"advertiser,omitempty"`
+
+	Attributes  CreativeAttributes `json:"attributes,omitempty"`
+	Bans        CreativeBans       `json:"bans,omitempty"`
+	CategoryID  *int64             `json:"category,omitempty"`
+	ContentType *string            `json:"content_type,omitempty"`
+	FinalURL    *string            `json:"final_url,omitempty"`
+	Javascript  *string            `json:"javascript,omitempty"`
+	Size        *SizeRelation      `json:"size,omitempty"`
+	SizeBytes   *int64             `json:"size_bytes,omitempty"`
+	Types       CreativeTypes      `json:"types,omitempty"`
+
+	AuditedAt *Time `json:"audited_at,omitempty"`
+	CheckedAt *Time `json:"checked_at,omitempty"`
+	UpdatedAt *Time `json:"updated_at,omitempty"`
 }
