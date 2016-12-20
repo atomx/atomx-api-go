b79e69a568633fb4a526fce53c6b52b2f0c49f0c
Erik Dubbelboer <erik@dubbelboer.com>
2016-12-20 10:44:09 +0000

    Add advertiser.is_dsp

--- advertiser.go
+++ advertiser.go
@@ -3,5 +3,6 @@ package atomx
 type Advertiser struct {
 	ID      int64            `json:"id"`
 	Name    string           `json:"name,omitempty"`
+	IsDSP   bool             `json:"is_dsp,omitempty"`
 	Network *NetworkRelation `json:"network,omitempty"`
 }
