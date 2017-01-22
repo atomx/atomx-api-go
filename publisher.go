9387568b7f69cc5f6b7b1747da0afa4921f45973
Erik Dubbelboer <erik@dubbelboer.com>
2017-01-22 06:18:35 +0000

    Add publisher.banned

--- publisher.go
+++ publisher.go
@@ -6,4 +6,7 @@ type Publisher struct {
 	Name     string           `json:"name,omitempty"`
 	RevShare float64          `json:"revshare,omitempty"`
 	Network  *NetworkRelation `json:"network,omitempty"`
+
+	// This field is for atomx internal use.
+	Banned *int64 `json:"banned,omitempty"`
 }
