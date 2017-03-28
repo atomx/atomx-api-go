38055608ba349e1b1c9016aaa99e78c61d384520
Erik Dubbelboer <erik@dubbelboer.com>
2017-03-28 05:47:43 +0000

    Add advertiser.trusted

--- advertiser.go
+++ advertiser.go
@@ -4,5 +4,6 @@ type Advertiser struct {
 	ID      int64            `json:"id"`
 	Name    string           `json:"name,omitempty"`
 	IsDSP   bool             `json:"is_dsp,omitempty"`
+	Trusted bool             `json:"trusted,omitempty"`
 	Network *NetworkRelation `json:"network,omitempty"`
 }
