66abef0ef22be18f0b9a5c8a0405346ba823a904
Erik Dubbelboer <erik@dubbelboer.com>
2018-02-28 03:55:31 +0000

    Add Network.OnlyBuyFromSelf

--- network.go
+++ network.go
@@ -1,7 +1,8 @@
 package atomx
 
 type Network struct {
-	ID            int64                  `json:"id"`
-	Name          string                 `json:"name,omitempty"`
-	SellerProfile *SellerProfileRelation `json:"seller_profile,omitempty"`
+	ID              int64                  `json:"id"`
+	Name            string                 `json:"name,omitempty"`
+	SellerProfile   *SellerProfileRelation `json:"seller_profile,omitempty"`
+	OnlyBuyFromSelf *bool                  `json:"only_buy_from_self,omitempty"`
 }
